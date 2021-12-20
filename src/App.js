14977dfa4d74b4899b67a2a10a3a6de050c8e05d
p1yush <rr2rinku@gmail.com>
2021-12-20 09:26:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Section from "./Section";
-import skilladd from './skilladd';
-import Skills from "./Skills";
-import Contact from "./Contacts";
-
 
 
 function App() {
@@ -13,19 +9,6 @@ function App() {
     <>
     <Header />
     <Section />
-    <div className = "skills">
-    <h1>Skills</h1>
-    <div className = "skills-display">
-    {skilladd.map((add) => (
-      <Skills 
-        key = {add.id}
-        name = {add.name}
-        colors = {add.colors} 
-      />
-      ))}
-      </div>
-      </div>
-      <Contact />
     </>
   );
 }
--- src/Contacts.js
+++
@@ -1,9 +0,0 @@
-function Contact() {
-    return (
-        <div className = "contacts">
-        <h1>Contacts</h1>
-        </div>
-    )
-}
-
-export default Contact;
