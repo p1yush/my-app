697668a05f9794ecb4b7f8b4f70b0d5d3fc68202
p1yush <rr2rinku@gmail.com>
2021-12-24 15:55:29 +0530

    new

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,8 +1,33 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+width: 100vw;
+heifht: 100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+`
+const Main = styled.div`
+border: 2px solid black;
+color: black;
+padding: 2rem;
+width: 30vw;
+height: 60vh;
+z-index: 3;
+line-height: 1.5;
+display: flex;
+flex-direction: column;
+justfy_content: space-between;
+`
+
 function Skill() {
     return (
-        <div>
-            Skill Page
-        </div>
+        <Box>
+            <Main>
+
+            </Main>
+        </Box>
     )
 }
 export default Skill;
