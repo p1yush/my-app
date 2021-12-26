210b8d432dccf8442b1eaee80b04f3b121b19e29
p1yush <rr2rinku@gmail.com>
2021-12-26 15:57:15 +0530

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -9,12 +9,33 @@ height:100vh;
 position: relative;
 overflow: hidden;
 `
+const Main = styled.div`
+border: 2px solid;
+color: black;
+padding: 2rem;
+width: 50vw;
+hight: 60vh;
+z-index: 3;
+line-height: 3;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: calc(0.6rem + 1vh);
+backdrop-filter: blur(4px);
+position: absolute;
+left: 10rem;
+top: 10rem;
+`
+
 
 function About() {
     return (
         <Box>
             <ParticleComponent2 />
             <SocailIcons />
+            <Main>
+                hjkhsnkhekdlsjposd
+            </Main>
             
         </Box>
     )
